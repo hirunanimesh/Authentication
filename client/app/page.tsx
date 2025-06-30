56503ae0171dcb8a06390eb1bcf42318032de8f7
hirunanimesh <158060469+hirunanimesh@users.noreply.github.com>
2025-06-30 21:03:05 +0530

    initiate the project

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+      <h1 className="text-4xl font-extrabold text-blue-600 underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
